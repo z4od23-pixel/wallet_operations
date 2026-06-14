71b44dc18e036c77436850d1465927349f09f969
Ilia Zakharov <z4od23@gmail.com>
2026-06-14 16:29:45 +0500

    Изменил метод get на правильный

--- models/wallet.py
+++ models/wallet.py
@@ -2,9 +2,7 @@ from database import Model
 from sqlalchemy.orm import Mapped, mapped_column
 
 class WalletMOdel(Model):
-    __tablename__ = "operations"
+    __tablename__ = "wallet"
 
     wallet_id : Mapped[int] = mapped_column(primary_key=True, init=False)
     balance : Mapped[int]
-    operation_type : Mapped[str] 
-    amount : Mapped[int]
--- router/router.py
+++ router/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException,status
 from sqlalchemy import select
 from shemas.shemas import WalletBase,WalletOperation
 from models.wallet import WalletMOdel
@@ -7,9 +7,16 @@ from database import SessionDep
 router = APIRouter(prefix="/api/v1/wallets", tags=["Операции кошелька"])
 
 @router.get("/{wallet_id}")
-async def get_balance(wallet_id : int, session : SessionDep) ->int :
-    query = select(WalletMOdel.balance).where(WalletMOdel.wallet_id==wallet_id)
+async def get_balance(wallet_id : int, session : SessionDep)  :
+    check_query = select(WalletMOdel)
+    check_result = await session.execute(check_query)
+    check_id = check_result.scalars().all()
+    if len(check_id) < wallet_id :
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='Кошелек с таким id не найден !')
+    
+    query = select(WalletMOdel.balance).where(WalletMOdel.wallet_id == wallet_id)
     result = await session.execute(query)
     balance = result.scalars().all()
-    return balance[0]
+    return {"Баланс кошелька " :balance[0], " Wallet_id ": wallet_id}
 
+ 
